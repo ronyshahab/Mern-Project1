8d2c0c21a2ee870630eccac06b87fb4971c1f28c
ronyshahab <ronaksingh161005@google.com>
2023-02-18 13:29:25 +0530

    last commit

--- backend/config/connect.js
+++ backend/config/connect.js
@@ -1,18 +1,24 @@
-const mongoose = require('mongoose')
-const config = require('config')
+const mongoose = require("mongoose");
+const config = require("config");
 
-const DB = config.get('mongoUrl')
+const DB = config.get("mongoUrl");
 
-mongoose.set('strictQuery', true);
-const connectDB = async ()=>{
-    try {
-        
-        await mongoose.connect(DB)
-        console.log("Mongo connected succesfully")
-    } catch (error) {
-        console.log("Connection with Mongo failed")
-        process.exit(1)       
-    }
-}
+mongoose.set("strictQuery", true);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(DB);
+    console.log("Mongo connected succesfully");
+  } catch (error) {
+    console.log("Connection with Mongo failed");
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
+
+/*
+const mongoose = require("mongoDose)
+const config = require("config")
+
+
+*/

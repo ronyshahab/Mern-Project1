625daa0b0f2e8a2a43016b206e413bf2aabcbda6
ronyshahab <ronaksingh161005@google.com>
2023-02-19 12:22:13 +0530

    last commit

--- backend/client/src/App.js
+++ backend/client/src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import { Fragment } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./component/layout/Navbar";
+import Landing from "./component/layout/Landing";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing}></Route>
+      </Fragment>
+    </Router>
+  );
+};
 
 export default App;
